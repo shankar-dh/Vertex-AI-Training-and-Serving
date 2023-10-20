c36d8a78cc545af8ce708db2d6e933177d144cc2
Dhanush Kumar <shankar.dh@northeastern.edu>
2023-10-20 19:59:56 -0400

    added docker files and model files

--- src/dataflow.py
+++ src/dataflow.py
@@ -2,14 +2,22 @@ import apache_beam as beam
 from apache_beam.io import WriteToText
 from apache_beam.options.pipeline_options import PipelineOptions, GoogleCloudOptions
 import os
-import pandas as pd
 import json
+from apache_beam.dataframe.io import read_csv
+
+
+
+pipeline_options = PipelineOptions()
+
+
 
 class LoadDataFn(beam.DoFn):
     def process(self, element):
         file_path = element
+        with beam.Pipeline() as p:
+            df = p | read_csv(file_path)
 
-        df = pd.read_csv(file_path)
+        # df = pd.read_csv(file_path)
         return [df]
 
 class PreprocessDataFn(beam.DoFn):
@@ -39,26 +47,26 @@ from apache_beam.options.pipeline_options import SetupOptions  # add this import
 
 def run_pipeline():
     # Define GCP Dataflow options
-    # pipeline_options = PipelineOptions()
-    # pipeline_options.view_as(GoogleCloudOptions).project = 'weighty-forest-399219'
-    # pipeline_options.view_as(GoogleCloudOptions).job_name = 'timseseries'
-    # pipeline_options.view_as(GoogleCloudOptions).staging_location = 'gs://mlops-data-ie7374/staging'
-    # pipeline_options.view_as(GoogleCloudOptions).temp_location = 'gs://mlops-data-ie7374/temp'
-    # pipeline_options.view_as(GoogleCloudOptions).region = 'us-east1'  # replace 'us-central1' with your desired region
-    # pipeline_options.view_as(StandardOptions).runner = 'DataflowRunner'
-
-     # Define pipeline options for local execution
     pipeline_options = PipelineOptions()
-    pipeline_options.view_as(StandardOptions).runner = 'DirectRunner'  # Set to DirectRunner for local execution
+    pipeline_options.view_as(GoogleCloudOptions).project = 'weighty-forest-399219'
+    pipeline_options.view_as(GoogleCloudOptions).job_name = 'timseseries'
+    pipeline_options.view_as(GoogleCloudOptions).staging_location = 'gs://mlops-data-ie7374/staging'
+    pipeline_options.view_as(GoogleCloudOptions).temp_location = 'gs://mlops-data-ie7374/temp'
+    pipeline_options.view_as(GoogleCloudOptions).region = 'us-east1'
+    pipeline_options.view_as(StandardOptions).runner = 'DataflowRunner'
+
+    #  # Define pipeline options for local execution
+    # pipeline_options = PipelineOptions()
+    # pipeline_options.view_as(StandardOptions).runner = 'DirectRunner'  # Set to DirectRunner for local execution
 
     setup_options = pipeline_options.view_as(SetupOptions)
-    setup_options.setup_file = os.path.abspath('setup.py')
+    setup_options.requirements_file = "requirements.txt"
 
     with beam.Pipeline(options=pipeline_options) as p:
         # Paths to GCS
         input_data = (p | "ReadInputData" >> beam.Create([
-            # 'gs://mlops-data-ie737/data/train/train_data.csv'
-            os.path.join("..", "data", "train", "train_data.csv")
+            'gs://mlops-data-ie737/data/train/train_data.csv'
+            # os.path.join("..", "data", "train", "train_data.csv")
         ]))
 
 
@@ -69,13 +77,16 @@ def run_pipeline():
         # Write the normalized training data to a CSV file.
         (preprocessed_data | "WriteTrainData" >> beam.Map(lambda x: x[0])  
                            | "WriteToCSV" >> beam.Map(lambda df: df.to_csv(
-                            #    'gs://mlops-data-ie7374/data/train/train_normalized.csv',
-                            os.path.join("..", "data", "train", "train_normalized.csv"),
+                               'gs://mlops-data-ie7374/data/train/train_normalized.csv',
+                            # os.path.join("..", "data", "train", "train_normalized.csv"),
                                  index=False)))
 
         # Write normalization stats to a JSON file using Beam's WriteToText transform
         (preprocessed_data | "ExtractNormalizationStats" >> beam.Map(lambda x: json.dumps(x[1]))
-                        | "WriteToJson" >> WriteToJson(os.path.join("..", "data", "normalization_stats")))
+                        | "WriteToJson" >> WriteToJson(
+                            'gs://mlops-data-ie7374/data/normalization_stats'
+                            # os.path.join("..", "data", "normalization_stats"
+                                         ))
 
 
 if __name__ == "__main__":
