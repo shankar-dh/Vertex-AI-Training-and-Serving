4ddf987ab1bccdc7c9499abc1841c6e12e64fee6
DRAJ6 <144625117+DRAJ6@users.noreply.github.com>
2023-11-16 20:42:30 -0500

    Update train.py

--- src/trainer/train.py
+++ src/trainer/train.py
@@ -95,7 +95,7 @@ def data_transform(df):
     client = storage.Client()
     bucket_name = os.getenv("BUCKET_NAME")
     blob_path = 'scaler/normalization_stats.json' # Change this to your blob path where the data is stored
-    bucket = client.get_bucket(bucket_name)
+    bucket = client.get_bucket("mlops_fall23")
     blob = bucket.blob(blob_path)
 
     # Download the json as a string
